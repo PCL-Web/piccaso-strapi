392104b1679a3bb0bfaed3007c788c439119a0be
PCL-Web <125276029+PCL-Web@users.noreply.github.com>
2023-07-17 19:50:26 +0100

    Update database.js

--- config/database.js
+++ config/database.js
@@ -2,12 +2,12 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      url: env('DATABASE_URL','postgresql://postgres:d8XFJUnCTeaGdomQFXIk@containers-us-west-91.railway.app:7871/railway'),
-      host: env('PGHOST', 'containers-us-west-91.railway.app'),
-      port: env.int('PGPORT', 7871),
+      url: env('DATABASE_URL','postgresql://postgres:ONLlSaSsmhdCL704fpzG@containers-us-west-4.railway.app:6008/railway'),
+      host: env('PGHOST', 'containers-us-west-4.railway.app'),
+      port: env.int('PGPORT', 6008),
       database: env('PGDATABASE', 'railway'),
       user: env('PGUSER', 'postgres'),
-      password: env('PGPASSWORD', 'd8XFJUnCTeaGdomQFXIk'),
+      password: env('PGPASSWORD', 'ONLlSaSsmhdCL704fpzG'),
       ssl: env.bool(true),
     },
     pool: { min: 0 }
