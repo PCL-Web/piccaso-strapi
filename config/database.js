949765fb81dbd8db980ce902d41547e5aaa4817f
John Watson <john.watson@privacyculture.com>
2023-07-17 19:38:16 +0100

    Page schema

--- config/database.js
+++ config/database.js
@@ -2,11 +2,12 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('PGHOST', '127.0.0.1'),
-      port: env.int('PGPORT', 5432),
-      database: env('PGDATABASE', 'strapi'),
-      user: env('PGUSER', 'strapi'),
-      password: env('PGPASSWORD', 'password'),
+      ur: env('DATABASE_URL', 'postgresql://postgres:d8XFJUnCTeaGdomQFXIk@containers-us-west-91.railway.app:7871/railway'),
+      host: env('PGHOST', 'containers-us-west-91.railway.app'),
+      port: env.int('PGPORT', 7871),
+      database: env('PGDATABASE', 'railway'),
+      user: env('PGUSER', 'postgres'),
+      password: env('PGPASSWORD', 'd8XFJUnCTeaGdomQFXIk'),
       ssl: env.bool(true),
     },
     pool: { min: 0 }
