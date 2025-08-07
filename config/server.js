fbb83cf1ab1daa647a3aecc3b599a695aa8849c9
PrivacyCulture <145655975+PrivacyCulture@users.noreply.github.com>
2025-08-07 20:27:54 +0100

    Load error fix

--- config/server.js
+++ config/server.js
@@ -1,13 +1,10 @@
 const fs = require('fs');
 const path = require('path');
 
-
-
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: 1337,
+  port: env.int('PORT', 1337),
   app: {
     keys: env.array('APP_KEYS'),
   }
 });
-
